57d798c4e9950be449aaf1e98a2e301c68868569
Patricia Nissen <pat.nissen87@mail.de>
2024-04-16 18:42:08 +0200

    first

--- werwirdmill/src/views/HomeView.vue
+++ werwirdmill/src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <template>
   <main>
-   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero, libero explicabo! Eaque, quia quis! Impedit in enim libero corrupti, voluptas quos incidunt rem dicta unde reiciendis a mollitia tempora, ipsum numquam natus possimus laudantium tenetur velit quasi esse distinctio adipisci? Debitis minus ullam recusandae laborum, cumque cupiditate iste doloremque iure.
-
+  START
 
 
   </main>
