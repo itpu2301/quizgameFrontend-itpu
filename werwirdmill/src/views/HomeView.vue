ee100a46df25b97578416cd72480300a55346d7f
Patricia Nissen <pat.nissen87@mail.de>
2024-04-16 18:16:35 +0200

    first

--- werwirdmill/src/views/HomeView.vue
+++ werwirdmill/src/views/HomeView.vue
@@ -1,12 +1,8 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero, libero explicabo! Eaque, quia quis! Impedit in enim libero corrupti, voluptas quos incidunt rem dicta unde reiciendis a mollitia tempora, ipsum numquam natus possimus laudantium tenetur velit quasi esse distinctio adipisci? Debitis minus ullam recusandae laborum, cumque cupiditate iste doloremque iure.
 
 
-    
+
   </main>
 </template>
