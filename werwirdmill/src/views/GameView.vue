cd3310282a988760960be66ff977215e1764f899
Patricia Nissen <pat.nissen87@mail.de>
2024-04-18 18:07:02 +0200

    second

---
+++ werwirdmill/src/views/GameView.vue
@@ -0,0 +1,69 @@
+<template>
+    <!-- <nav>
+        <div class="column-4 home-1">
+            <h2 class="home-text">
+                <RouterLink to="/">Home-Screen</RouterLink>
+            </h2>
+        </div>
+    </nav> -->
+
+    <main>
+
+        <div class="logo"></div>
+        
+        <div class="parent">
+            <div class="div1">
+                <div class="title">
+                Willkommen XXX, zu Wer wird Millionär!
+            </div>
+            </div>
+            <div class="div2">
+                <div class="question">
+                    Frage: Wird ein Roman häppchenweise in der Tageszeitung abgedruckt, steht am Ende eines Teils
+                    meist?
+                </div>
+            </div>
+            <div class="div3">
+                <div>
+                    <div class="answers"> A: Fortsetzung folgt </div>
+                    <div class="answers"> C: Die Spannung steigt </div>
+                </div>
+                <div>
+                    <div class="answers"> B: Bleiben Sie dran! </div>
+                    <div class="answers"> D: Ende offen </div>
+                </div>
+            </div>
+            <div class="div4">
+                <div class="gewinn">
+
+                    1.000.000 Euro<br>
+                    500.000 Euro<br>
+                    125.000 Euro<br>
+                    64.000 Euro<br>
+                    32.000 Euro<br>
+                    --- <br>
+                    16.000 Euro<br>
+                    8000 Euro<br>
+                    4000 Euro<br>
+                    300 Euro<br>
+                    2000 Euro<br>
+                    --- <br>
+                    1000 Euro<br>
+                    500 Euro<br>
+                    200 Euro<br>
+                    100 Euro<br>
+                    50 Euro
+
+                </div>
+            </div>
+        </div>
+
+
+
+    </main>
+</template>
+      
+
+<style>
+@import "@/assets/game/game.css";
+</style>
--- werwirdmill/src/views/HomeView.vue
+++ werwirdmill/src/views/HomeView.vue
@@ -1,4 +1,12 @@
 <template>
+  <nav>
+
+<div class="column-4 home-1">
+  <h2 class="home-text">
+    <RouterLink to="/game">asd</RouterLink>
+  </h2>
+</div>
+</nav>
   <main>
   START
 
