1aaa30617cf83c39b2d8ff852c4a0ffe4750ecd5
Beau <114217941+Beau735@users.noreply.github.com>
2024-04-26 18:38:24 +0200

    HomeView.vue

--- werwirdmill/src/views/HomeView.vue
+++ werwirdmill/src/views/HomeView.vue
@@ -1,15 +1,31 @@
 <template>
-  <nav>
-
-<div class="column-4 home-1">
-  <h2 class="home-text">
-    <RouterLink to="/game">asd</RouterLink>
-  </h2>
-</div>
-</nav>
   <main>
-  START
-
-
+    <div class="page">
+      <img src="../assets/logo.png" alt="brand-logo" id="brandLogo"/>
+      <h3>Wer spielt&quest;</h3>
+      <input id="player" placeholder="Name">
+      <button class="close">
+        <RouterLink to="/game" id="play">Spielen</RouterLink>
+      </button>
+    </div>
+    <div class="gradient"></div>
   </main>
 </template>
+
+<script lang="js">
+export default {
+  mounted() {
+    document.addEventListener("mousemove", this.onMouseMove);
+  },
+  methods: {
+    onMouseMove(event) {
+      const gradient = document.querySelector(".gradient");
+      gradient.style.backgroundImage = `radial-gradient(at ${event.clientX}px ${event.clientY}px, rgba(159,0,191,.9) 0, #4D4FA7 70%)`;
+    },
+  },
+};
+</script>
+
+<style>
+@import "@/assets/home.css";
+</style>
